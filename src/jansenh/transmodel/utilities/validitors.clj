26fef4ed12e11d0a6a1d1acbccdb530fd741209f
Henning Jansen <jansenh@jansenh-xps13.jansenh.no>
2025-12-22 20:27:07 +0100

    Add utilities/validators

---
+++ src/jansenh/transmodel/utilities/validitors.clj
@@ -0,0 +1,38 @@
+;;-----------------------------------------------------------------------------
+;; File: src/jansenh/transmodel/utilities/validators.clj
+;; Author: Henning Jansen - henning.jansen@jansenh.no
+;; Copyright: (c) 2025
+;; License: Eclipse Public License 2.0 - http://www.eclipse.org/legal/epl-2.0.
+;;
+;;-----------------------------------------------------------------------------
+
+(ns jansenh.transmodel.utilities.validitors)
+
+;; Transmodel validator functions
+;;
+;; authors: Henning Jansen - henning.jansen@norled.no
+;; version: 0.2.0 2025-12-19
+;; since: 0.2.0 2025-12-19
+;; -----------------------------------------------------------------------------
+
+(defn valid-date-time?
+  "Check if timestamp is valid ISO-8601 local date time.
+
+   This is what we must anticipate for validity within the NeTEx and SIRI
+   standards echo-system. Go see the tests!
+
+   Returns: truthy on valid timestamp
+  "
+  [timestamp]
+   ;; This function will probably be duplicated into src/ at some point. Let's
+   ;; keep that duplication if it happens; I wanted to tie this test for validity
+   ;; inside scope of tests for obvious and non-obvious reasons:
+   ;; - verbosity (if needed)
+   ;; - evolving edge cases
+  (if (nil? timestamp)
+    nil
+    (re-matches #"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}"
+                timestamp)))
+
+
+
