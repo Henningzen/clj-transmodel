e1a9134aebcf6c16e58b129ee20bd782d6533299
Henning Jansen <henning.jansen@jansenh.no>
2026-01-14 13:21:56 +0100

    add new namespaces

---
+++ src/jansenh/transmodel/core.clj
@@ -0,0 +1,8 @@
+;;-----------------------------------------------------------------------------
+;; File: src/jansenh/transmodel/core.clj
+;; Author: Henning Jansen - henning.jansen@jansenh.no
+;; Copyright: (c) 2025
+;; License: Eclipse Public License 2.0 - http://www.eclipse.org/legal/epl-2.0.
+;;-----------------------------------------------------------------------------
+
+(ns jansenh.transmodel.core)
