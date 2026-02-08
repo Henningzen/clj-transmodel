ac6187ed06743e3884bc97da1059b72cc1a3f978
Henning Jansen <henning.jansen@jansenh.no>
2026-02-08 11:09:18 +0100

    refactor: Cleanup the Notebook code

--- notebooks/netex.clj
+++ notebooks/netex.clj
@@ -1,12 +1,6 @@
 (ns notebooks.netex
   (:require [jansenh.transmodel.parser.core :as parser]
-            [jansenh.transmodel.parser.calendar :as cal]
-            [jansenh.transmodel.generator.timetable :as tt]
-            [jansenh.transmodel.netex.registry :as reg]
-            [jansenh.transmodel.netex.line :as line]
-            [jansenh.transmodel.netex.explore :as exp]
-            [jansenh.transmodel.netex.extract :as ext]
-            [jansenh.transmodel.netex.interchanges :as intrc]
+            [jansenh.transmodel.netex.calendar :as cal]
             [scicloj.clay.v2.api :as clay]
             [scicloj.clay.v2.main]
             [scicloj.kindly.v4.kind :as kind]
