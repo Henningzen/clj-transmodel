1ceebcd8d03c0598e7b7cda4b9eea9c334c1c6b9
Henning Jansen <henning.jansen@jansenh.no>
2026-04-21 21:42:59 +0200

    wip: Netex Notebook

--- notebooks/netex.clj
+++ notebooks/netex.clj
@@ -1,66 +1,87 @@
-(ns notebooks.netex
+(ns netex
   (:require [jansenh.transmodel.parser.core :as parser]
             [jansenh.transmodel.netex.calendar :as cal]
             [jansenh.transmodel.parser.utilities :as utils]
+            [jansenh.transmodel.api :as api]
             [scicloj.clay.v2.api :as clay]
-            [scicloj.clay.v2.main]
-            [scicloj.kindly.v4.kind :as kind]
-            [tablecloth.api :as tc]
-            [clojure.string :as str]))
+            [tablecloth.api :as tc]))
 
 
 ;;  NeTEx explore notebook
 ;;  ======================
 ;;
-;;    Notebook for NeTEx dataset exploration.
+;;  Notebook for NeTEx dataset exploration
+;;  --------------------------------------
 ;;
-;;    The implementation is based on Clojure with Noj, Clay, TableCloth et al.
-;;    Visualization tools in use are from the JavaScript sphere of tooling.
+;;  The implementation is based on Clojure with Noj, Clay, TableCloth et al.
+;;  Visualization tools in use are from the JavaScript sphere of tooling.
 ;;
-;;    The api refered in the :require section is a wrapper of a NeTEx parsing
-;;    library https://github.com/Henningzen/clj-transmodel
-;;
-;;    The library is available under EPL 2.0 License but not yet made public.
+;;  The `api` refered to in the :require section is a wrapper of a NeTEx parsing
+;;  library https://github.com/Henningzen/clj-transmodel
 ;;
 ;; -----------------------------------------------------------------------------
-;;  Henning Jansen 2025  Copyright © henning.jansen@jansenh.no
+;;  Copyright © Henning Jansen henning.jansen@jansenh.no 2025-2026  
 ;;  Distributed under the Eclipse Public License version 2.0 as
 ;;  described in the README file under the root of this project.
+;;
+;; -----------------------------------------------------------------------------
+;;
 
-
-(def shared-data-file "/home/jansenh/data/netex/KOL/_KOL_shared_data.xml")
-(def line-data-file
-  "/home/jansenh/data/netex/KOL/KOL_KOL-Line-8_5986_1025_Fogn---Judaberg---Helgoy.xml")
-
-(def shared-data (parser/parse-xml-file shared-data-file))
-(def line-data (parser/parse-xml-file line-data-file))
-
-(def date-range (cal/weeks-ahead 1))
-
+(def shared-data (parser/parse-xml-file "/home/jansenh/data/netex/KOL/_KOL_shared_data.xml"))
 
 (def calendar-index (cal/build-calendar-index shared-data))
-(def calendar-stats (:stats calendar-index))
 
-(keys calendar-index)
-(count (:operating-periods calendar-index))
-(count (:day-types calendar-index))
+(def lines api/lines)
 
-(-> date-range
+(-> (cal/weeks-ahead 1)
     (update :from utils/local-date->str)
     (update :to   utils/local-date->str)
     (tc/dataset        {:dataset-name "Date range"})
     (tc/rename-columns {:from "From" :to "To"}))
 
-(-> calendar-stats
-    (tc/dataset        {:dataset-name "Calendar statistics"})
-    (tc/rename-columns {:day-type-count "day-type count" :period-count "period count" :assignement-count "assignement count"}))
+;; -----------------------------------------------------------------------------
+
+(-> (:stats calendar-index)
+    (tc/dataset        {:dataset-name      "Calendar statistics"})
+    (tc/rename-columns {:day-type-count    "day-type count"
+                        :period-count      "period count"
+                        :assignement-count "assignement count"}))
 
 ;; -----------------------------------------------------------------------------
 
+(-> (first lines)
+    (as-> m (into {} (remove (comp nil? val)) m)) ;; TODO!
+    (tc/dataset        {:dataset-name   "Line data"})
+    (tc/rename-columns {:id             "Id"
+                        :version        "Version"
+                        :name           "Name"
+                        :public-code    "Public code"
+                        :private-code   "Private code"
+                        :operator-ref   "Operator ref"
+                        :transport-mode "Transport mode"}))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 (comment
+  
   "Clay specifics
    --------------
    - rendering and behaviour"
+
   (clay/make! {:format [:html]
                :source-path "notebooks/netex.clj"
                :title "NeETx Transmodel"
@@ -70,6 +91,9 @@
                :hide-ui-header false
                :hide-info-line true
                :hide-code true})
+  
   ;;------------------------------------------------------------------>  comment
   ;;
   )
+
+
